ce0f10b0625eafcfa13f1a74d695914de131ee09
poke1337 <martin.e.tons@gmail.com>
2016-08-06 12:14:52 +0200

    Updated to use hla hooks.

--- gamemode/modules/game/sh_start.lua
+++ gamemode/modules/game/sh_start.lua
@@ -4,9 +4,11 @@ local fProcentVote	= 0.67
 if CLIENT then return end --> Remove this and the locals above when we get a settings file. And change the file to a server file.
 
 hla.CreateHook("RoundStart")
+hla.CreateHook("PlayerDisconnected") --> Use on player disconnect hook.
+hla.CreateHook("ShowSpare2") --> Use the F4 hook.
 
 --> Remove a players vote if they voted and then disconnected.
-hook.Add("PlayerDisconnected", "Remove start vote", function(ply)
+hla.AddHook("PlayerDisconnected", "Remove start vote", function(ply)
 
 	for i=1,#iVoted do
 
@@ -22,7 +24,7 @@ hook.Add("PlayerDisconnected", "Remove start vote", function(ply)
 end)
 
 --> Player vote to start the round.
-hook.Add("ShowSpare2", "Vote to start button", function(ply)
+hla.AddHook("ShowSpare2", "Vote to start button", function(ply)
 
 	table.insert(iVoted, ply)
 
