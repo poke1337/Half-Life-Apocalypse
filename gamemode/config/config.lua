4d02d3e5937eeaa477faec28136eea003e25a549
Promptitude <ihasgood@gmail.com>
2016-08-06 21:12:40 +1000

    Changes
    Added config,
    Fixed sh_start

---
+++ gamemode/config/config.lua
@@ -0,0 +1,7 @@
+hla.Settings = {}
+
+hla.Settings[ "RunSpeed" ] = 300
+hla.Settings[ "WalkSpeed" ] = 200
+hla.Settings[ "JumpSpeed" ] = 100
+hla.Settings[ "DuckSpeed" ] = 0.2
+hla.Settings[ "UnDuckSpeed" ] = 0.4
--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,4 +1,9 @@
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 
+hla = hla or {}
+
+AddCSLuaFile( "config/config.lua" )
+
 include( "shared.lua" )
+include( "config/config.lua" )
