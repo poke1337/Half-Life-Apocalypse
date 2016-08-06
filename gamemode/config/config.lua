f41a1e5be0913d7a278fd2457bafde0c887132a3
poke1337 <martin.e.tons@gmail.com>
2016-08-06 13:20:48 +0200

    Added iVoted and fProcentVote to the config

--- gamemode/config/config.lua
+++ gamemode/config/config.lua
@@ -1,7 +1,12 @@
 hla.Settings = {}
 
-hla.Settings[ "RunSpeed" ] = 300
-hla.Settings[ "WalkSpeed" ] = 200
-hla.Settings[ "JumpSpeed" ] = 100
-hla.Settings[ "DuckSpeed" ] = 0.2
+--> Player movement
+hla.Settings[ "RunSpeed" ] 		= 300
+hla.Settings[ "WalkSpeed" ] 	= 200
+hla.Settings[ "JumpSpeed" ] 	= 100
+hla.Settings[ "DuckSpeed" ] 	= 0.2
 hla.Settings[ "UnDuckSpeed" ] = 0.4
+
+--> Round start voting
+hla.Settings[ "iVoted" ]				= {}
+hla.Settings[ "fProcentVote" ] 	= 0.67
