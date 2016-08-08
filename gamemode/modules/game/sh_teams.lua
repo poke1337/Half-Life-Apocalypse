5bf0dd64283a03436e10ef006f2d73c6c3316580
Promptitude <ihasgood@gmail.com>
2016-08-08 21:47:00 +1000

    test

--- gamemode/modules/game/sh_teams.lua
+++
@@ -1,3 +0,0 @@
-team.SetUp(0, "Joining", Color( 20, 20, 20 ), false)
-team.SetUp(1, hla.Settings["Team1N"], hla.Settings["Team1C"], false)
-team.SetUp(2, hla.Settings["Team2N"], hla.Settings["Team2C"], false)
